c50dda9ff2c04cabb9c72193182436698791d0a8
opusvar <sprojdev@gmail.com>
2021-08-31 09:58:00 -0400

    Added draft html, added save page function

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	// "html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -30,6 +31,14 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
 	fmt.Fprint(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
